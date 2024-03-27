d7e94bf8876beae2242f72fc4a8df2bcbca064d4
Areesha-sites <areesha21314@gmail.com>
2024-03-27 11:13:35 -0700

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+// 1) Computer will generate a random number - Done.
+// 2) User input for guessing number - Done.
+// 3) Compare user input with computer generated number and show result - Done.
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 console.log("Welcome to Number Guessing Game");
 const answer = await inquirer.prompt([
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+// 1) Computer will generate a random number - Done.
+// 2) User input for guessing number - Done.
+// 3) Compare user input with computer generated number and show result - Done.
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 console.log("Welcome to Number Guessing Game");
