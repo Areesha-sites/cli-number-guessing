9f9de9e6baca26d7455949916fc5cc20710825b5
Areesha-sites <areesha21314@gmail.com>
2024-06-13 12:02:30 -0700

    added figlet and ora

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-// 1) Computer will generate a random number - Done.
-// 2) User input for guessing number - Done.
-// 3) Compare user input with computer generated number and show result - Done.
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-console.log(chalk.cyan("Welcome to Number Guessing Game"));
-const answer = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: chalk.yellow("Please guess a number between 1-6:"),
-    },
-]);
-if (answer.userGuessedNumber === randomNumber) {
-    console.log(chalk.bold.magenta("Congratulations! you guessed right number"));
+import figlet from "figlet";
+import ora from "ora";
+// Function to start the game
+function startGame() {
+    console.log(chalk.cyan(figlet.textSync("Number Guessing Game")));
+    let score = 0;
+    let playAgain = true;
+    const spinner = ora(chalk.green("Loading game...")).start();
+    setTimeout(() => {
+        spinner.stop();
+        playGame();
+    }, 2000);
+    async function playGame() {
+        while (playAgain) {
+            const randomNumber = Math.floor(Math.random() * 6 + 1);
+            const answer = await inquirer.prompt([
+                {
+                    name: "userGuessedNumber",
+                    type: "number",
+                    message: chalk.yellow("Please guess a number between 1-6:"),
+                },
+            ]);
+            if (answer.userGuessedNumber === randomNumber) {
+                console.log(chalk.bold.magenta("Congratulations! You guessed the right number."));
+                score++;
+            }
+            else {
+                console.log(chalk.red(`You guessed the wrong number. The correct number was ${randomNumber}.`));
+            }
+            console.log(chalk.green(`Your current score is: ${score}`));
+            const playAgainAnswer = await inquirer.prompt([
+                {
+                    name: "playAgain",
+                    type: "confirm",
+                    message: "Do you want to play again?",
+                    default: false,
+                },
+            ]);
+            playAgain = playAgainAnswer.playAgain;
+        }
+        console.log(chalk.cyan("Thank you for playing! Your final score is: " + score));
+    }
 }
-else {
-    console.log(chalk.red("You guessed wrong number"));
-}
+startGame();
--- index.ts
+++ index.ts
@@ -2,23 +2,63 @@
 
 import inquirer from "inquirer";
 import chalk from "chalk";
-// 1) Computer will generate a random number - Done.
-// 2) User input for guessing number - Done.
-// 3) Compare user input with computer generated number and show result - Done.
+import figlet from "figlet";
+import ora, { Ora } from "ora";
 
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-console.log(chalk.cyan("Welcome to Number Guessing Game"));
+// Function to start the game
+function startGame(): void {
+  console.log(chalk.cyan(figlet.textSync("Number Guessing Game")));
+  let score: number = 0;
+  let playAgain: boolean = true;
 
-const answer = await inquirer.prompt([
-  {
-    name: "userGuessedNumber",
-    type: "number",
-    message: chalk.yellow("Please guess a number between 1-6:"),
-  },
-]);
+  const spinner: Ora = ora(chalk.green("Loading game...")).start();
+  setTimeout(() => {
+    spinner.stop();
+    playGame();
+  }, 2000);
 
-if (answer.userGuessedNumber === randomNumber) {
-  console.log(chalk.bold.magenta("Congratulations! you guessed right number"));
-} else {
-  console.log(chalk.red("You guessed wrong number"));
+  async function playGame() {
+    while (playAgain) {
+      const randomNumber: number = Math.floor(Math.random() * 6 + 1);
+      const answer = await inquirer.prompt([
+        {
+          name: "userGuessedNumber",
+          type: "number",
+          message: chalk.yellow("Please guess a number between 1-6:"),
+        },
+      ]);
+
+      if (answer.userGuessedNumber === randomNumber) {
+        console.log(
+          chalk.bold.magenta("Congratulations! You guessed the right number.")
+        );
+        score++;
+      } else {
+        console.log(
+          chalk.red(
+            `You guessed the wrong number. The correct number was ${randomNumber}.`
+          )
+        );
+      }
+
+      console.log(chalk.green(`Your current score is: ${score}`));
+
+      const playAgainAnswer = await inquirer.prompt([
+        {
+          name: "playAgain",
+          type: "confirm",
+          message: "Do you want to play again?",
+          default: false,
+        },
+      ]);
+
+      playAgain = playAgainAnswer.playAgain;
+    }
+
+    console.log(
+      chalk.cyan("Thank you for playing! Your final score is: " + score)
+    );
+  }
 }
+
+startGame();
